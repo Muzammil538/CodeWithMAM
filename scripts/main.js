d02026db124e7f318b27383d1831548ae3690835
Muzammil538 <70688333+Muzammil538@users.noreply.github.com>
2021-08-06 11:08:05 +0530

    changed

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,17 @@
-const VisitEl = document.getElementById("visit");
-
-VisitCountor();
-
-function VisitCountor () {
-    fetch('https://api.countapi.xyz/update/carryminati/youtube/?amount=1').then(res => res.json()).then(res => {
-        VisitEl.innerHTML = res.value;
-    });
-
-}
+const Nav = document.querySelector(".nav_links");
+const HamBurger = document.querySelector(".hamburger");
+HamBurger.addEventListener("click" , () => {
+    Nav.classList.toggle("active");
+});
+// Change the Theme 
+const Icon = document.getElementById("icon");
+Icon.addEventListener("click", () => {
+    document.body.classList.toggle("theme");    
+    if(document.body.classList.contains("theme")){
+        Icon.classList.remove("fa-moon");
+        Icon.classList.add("fa-sun");   
+    }else{
+        Icon.classList.remove("fa-sun");
+        Icon.classList.add("fa-moon");
+    }
+})
